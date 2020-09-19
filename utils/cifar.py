8f20956873ce274ce97ee528cffe0b21b20190eb
root <root@gke-us-central1-v3-v100x8-prem-v1-9c3313dd-x15j.c.uber-ma.internal>
2020-09-19 23:50:16 +0000

    update

--- utils/cifar.py
+++ utils/cifar.py
@@ -28,8 +28,8 @@ class IndexedCifar10(Dataset):
         self.cifar10 = datasets.CIFAR10(
             root=root, download=download, train=train, transform=transform)
         
-        #if train:
-        #    self.random_labels = np.load('dataset_overrides/cifar/50pct_random_label.npy')
+        if train:
+            self.random_labels = np.load('dataset_overrides/cifar10/25pct_random_label.npy')
         
     def __getitem__(self, index):
         data, target = self.cifar10[index]
