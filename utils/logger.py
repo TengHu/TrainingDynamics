41994ca36fc753cec34a55b2e2a3a16de73ca31b
Niel Hu <niel.hu@uber.com>
2020-10-13 15:50:13 -0700

    .

--- utils/logger.py
+++ utils/logger.py
@@ -86,7 +86,7 @@ class Logger(object):
     def append(self, numbers):
         assert len(self.names) == len(numbers), 'Numbers do not match names'
         for index, num in enumerate(numbers):
-            self.file.write("{0:.6f}".format(num + numbers[1]))
+            self.file.write("{0:.6f}".format(num))
             self.file.write('\t')
             self.numbers[self.names[index]].append(num)
         self.file.write('\n')
